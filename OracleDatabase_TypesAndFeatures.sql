a690ee991161415d243d5304779b9f987d2d8898
Bizzarro1 <117869411+Bizzarro1@users.noreply.github.com>
2022-11-14 14:30:46 +0100

    Create OracleDatabase_TypesAndFeatures.sql

---
+++ OracleDatabase_TypesAndFeatures.sql
@@ -0,0 +1,5 @@
+DBMS; Tipi_numerici; Intervallo; Memoria
+Oracle Database; NUMBER(p,s); 1.0x10^-130, 1.0x10^126 non incluso; 1-22bytes
+Oracle Database; FLOAT(p); 
+Oracle Database; BINARY_FLOAT; 32bit; 4bytes
+Oracle Database; BINARY_DOUBLE; 64bit; 8bytes
